87a133ddb39464c2b18441368394f31b161c0440
Yann-CV <yann.chene.tni@gmail.com>
2025-10-17 16:47:24 +0200

    Add the possibility to update some row if there is too many with the same value

--- goldener/pxt_utils.py
+++ goldener/pxt_utils.py
@@ -1,3 +1,6 @@
+from typing import Literal
+
+import pixeltable as pxt
 from pixeltable.catalog import Table
 from pixeltable.exprs import Expr
 
@@ -19,3 +22,79 @@ def get_expr_from_column_name(table: Table, column_name: str) -> Expr:
         raise ValueError(f"Column '{column_name}' does not exist in the table schema.")
 
     return getattr(table, column_name)
+
+
+def create_views_per_column_value(
+    table: Table,
+    col_expr: Expr,
+    if_exists: Literal["error", "ignore", "replace", "replace_force"] = "error",
+) -> list[str]:
+    """Create views for each distinct value in a specified column of a PixelTable table.
+
+    Args:
+        table: The PixelTable table.
+
+    Returns:
+        A list of view path corresponding to each label.
+    """
+    view_name_list = []
+    table_path_split = table.get_metadata()["path"].split(".")
+    table_dir = ".".join(table_path_split[:-1])
+    table_name = table_path_split[-1]
+    col_name = col_expr.display_str()
+    for col_value_dict in col_expr.distinct().collect():
+        col_value = col_value_dict[col_name]
+        view_name = f"{table_dir}.{table_name}_{col_name}_{col_value}"
+        pxt.create_view(
+            view_name, table.where(col_expr == col_value), if_exists=if_exists
+        )
+        view_name_list.append(view_name)
+
+    return view_name_list
+
+
+def set_value_to_full_column(
+    table: Table,
+    col_expr: Expr,
+    value: int | float | str,
+) -> None:
+    """Set a column to a specific value for all rows in a PixelTable table.
+
+    Args:
+        table: The PixelTable table.
+        col_expr: The column expression to be set.
+        value: The value to set the column to.
+    """
+    table.update({col_expr.display_str(): value})
+
+
+def set_value_to_idx_rows(
+    table: Table,
+    col_expr: Expr,
+    idx_list: list[int],
+    value: int | float | str,
+) -> None:
+    """Set a column to a specific value for rows with given indices in a PixelTable table.
+
+    Args:
+        table: The PixelTable table.
+        col_expr: The column expression to be set.
+        idx_list: List of row indices to update.
+        value: The value to set the column to.
+    """
+    table.where(table.idx.isin(idx_list)).update({col_expr.display_str(): value})
+
+
+def update_column_if_too_many(
+    table: Table,
+    col_expr: Expr,
+    value: int | float | str,
+    max_count: int,
+    new_value: int | float | str,
+) -> None:
+    value_df = table.where(col_expr == value)
+    value_count = value_df.select().count()
+    if value_count > max_count:
+        to_move = value_count - max_count
+        indices = [row["idx"] for row in value_df.sample(to_move).collect()]
+        set_value_to_idx_rows(table, col_expr, indices, new_value)
